fb14c1fc99f259de17ce56e546cb43eb7ff9a5ce
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2023-04-16 18:56:02 -0400

    added finished featuers (shuffleKeys and trailingCommas)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 string unformat(string json) {
@@ -84,4 +85,73 @@ string sPoNgEbObCaSe(string json) {
         }
     }
     return out.str();
+}
+
+bool equalToClose(char character) {
+  if (character == ']' || character == '}') {
+    return true;
+  }
+  return false;
+}
+
+string trailingCommas(string json) {
+  string commaString = "";
+  bool comma = true;
+  for (int i = 0; i < json.length(); i++) {
+    if (equalToClose(json[i]) && comma) {
+      commaString += ",";
+      comma = false;
+    } else if (equalToClose(json[i])) {
+      comma = true;
+    }
+    commaString += json[i];
+  }
+  return commaString;
+}
+
+vector<string> splitByChar(string str, char delim) {
+  vector<string> split;
+  string data;
+  stringstream ss(str);
+  while (getline(ss, data, delim)) {
+    split.push_back(data);
+  }
+  return split;
+}
+
+vector<string> makeShuffleableList(string json) {
+  string noBrackets = json.substr(1, json.length() - 2);
+  vector<string> splitData = splitByChar(noBrackets, ':');
+  vector<string> totalSplit;
+  for (int i = 0; i < splitData.size(); i++) {
+    vector<string> holder = splitByChar(splitData[i], ',');
+    for (int y = 0; y < holder.size(); y++) {
+      totalSplit.push_back(holder[y]);
+    }
+  }
+  return totalSplit;
+}
+
+string makeString(vector<string> vecstr) {
+  string total = "";
+  for (int i = 0; i < vecstr.size(); i++) {
+    total += vecstr[i];
+  }
+  return total;
+}
+
+string shuffleKeys(string json) {
+  vector<string> splitData = makeShuffleableList(json);
+  vector<string> random;
+  for (int i = 0; i < splitData.size(); i += 2) {
+    int num = rand() % 2;
+    if (num == 0) {
+      random.insert(random.begin(), splitData[i + 1]);
+      random.insert(random.begin(), splitData[i]);
+    } else {
+      random.push_back(splitData[i + 1]);
+      random.push_back(splitData[i]);
+    }
+  }
+  return makeString(random);
 }
